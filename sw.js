557ae46b88e8612dc108cd0f5fa4e78959eb445c
Tony Danielsen <295889824+tonyadanielsen-byte@users.noreply.github.com>
2026-08-27 16:40:17 +0200

    Make LOR service worker fully PWA capable

--- sw.js
+++ sw.js
@@ -1,11 +1,54 @@
+const CACHE_NAME = 'lor-shell-v3.8.8';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/lor-icon-192.png',
+  './icons/lor-icon-512.png'
+];
 const LOR_SCOPE = self.registration.scope;
 
 self.addEventListener('install', event => {
-  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put('./index.html', copy));
+          return response;
+        })
+        .catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      return response;
+    }))
+  );
 });
 
 self.addEventListener('notificationclick', event => {
@@ -46,8 +89,8 @@ try {
     const link = data.link || LOR_SCOPE;
     return self.registration.showNotification(title, {
       body,
-      icon:'./lor-shield-v385.svg',
-      badge:'./lor-shield-v385.svg',
+      icon:'./icons/lor-icon-192.png',
+      badge:'./icons/lor-icon-192.png',
       tag:data.tag || 'lor-notification',
       renotify:false,
       data:{ link, planId:data.planId || '', eventType:data.eventType || '' }
