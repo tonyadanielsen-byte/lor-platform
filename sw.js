6a6d5b239b36d6e3cb0800ab828fc1756154d3f5
Tony Danielsen <295889824+tonyadanielsen-byte@users.noreply.github.com>
2026-08-27 10:25:48 +0200

    Use v3.8.4 LOR icon for notifications

--- sw.js
+++ sw.js
@@ -46,8 +46,8 @@ try {
     const link = data.link || LOR_SCOPE;
     return self.registration.showNotification(title, {
       body,
-      icon:'./lor-shield-v379.svg',
-      badge:'./lor-shield-v379.svg',
+      icon:'./lor-shield-v384.svg',
+      badge:'./lor-shield-v384.svg',
       tag:data.tag || 'lor-notification',
       renotify:false,
       data:{ link, planId:data.planId || '', eventType:data.eventType || '' }
