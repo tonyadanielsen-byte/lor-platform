8e261bd195fbde9946475c80ecd31c98f5555043
Tony Danielsen <295889824+tonyadanielsen-byte@users.noreply.github.com>
2026-09-04 10:48:45 +0200

    Use repaired icons in service worker

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'lor-shell-v3.8.24';
+const CACHE_NAME = 'lor-shell-v3.8.26';
 const APP_SHELL = [
   './',
   './index.html',
   './manifest.webmanifest',
-  './icons/lor-icon-192-v3819.png',
-  './icons/lor-icon-512-v3819.png'
+  './icons/lor-icon-192-v3826.png',
+  './icons/lor-icon-512-v3826.png'
 ];
 const LOR_SCOPE = self.registration.scope;
 
@@ -110,8 +110,8 @@ try {
     const link = data.link || LOR_SCOPE;
     return self.registration.showNotification(title, {
       body,
-      icon:'./icons/lor-icon-192-v3819.png',
-      badge:'./icons/lor-icon-192-v3819.png',
+      icon:'./icons/lor-icon-192-v3826.png',
+      badge:'./icons/lor-icon-192-v3826.png',
       tag:data.tag || 'lor-notification',
       renotify:false,
       data:{ link, planId:data.planId || '', eventType:data.eventType || '' }
